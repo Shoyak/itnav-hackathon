3dcee1abc4642b1bc5753132d7d839c620fd990a
久保昇也 <skhcvhghdyjrt@gmail.com>
2018-02-08 10:33:27 +0900

    [Test] APIからデータを取得してindex.vueに表示

--- src/components/entry/Index.vue
+++ src/components/entry/Index.vue
@@ -1,7 +1,10 @@
 <template lang='pug'>
 .vue-index
     .reactive-title {{ reactiveTitle() }}
-    sample
+    h2 以下APIテスト
+    ul
+        li(v-for="character in characters") {{character.name}}
+    button(@click="getCharacters")  button
 </template>
 
 <script lang='ts'>
@@ -10,6 +13,8 @@ import Component from 'vue-class-component';
 import VueUtil from '@/scripts/util/VueUtil';
 import RootVue from '@/components/RootVue';
 import Sample from '@/components/Sample.vue';
+// Ajax通信ライブラリ
+import Axios from 'axios';
 
 /**
  * Vue Component
@@ -17,12 +22,33 @@ import Sample from '@/components/Sample.vue';
 @Component({})
 export default class Index extends RootVue {
     public title: string = 'index';
+    public characters = [
+        {"name": ""}
+    ];
 
     protected beforeCreate(): void {
         // Inner Vue 登録
         VueUtil.registerComponents([Sample]);
     }
+
+    // apiからaxiosを用いて取得
+    async getCharacters() {
+        let url = 'https://gist.githubusercontent.com/anonymous/c41ae1698aca3595b95d1496ebf42d83/raw/2addeb281bcb4aae2be9c8204c0ec623c4cb446c/characters.json';
+        // await Axios.get(url)
+        // .then(x => {
+        //     this.characters = x.data;
+        // });
+
+        // try chathバージョン
+        try {
+            let res = await Axios.get(url)
+            this.characters = res.data
+        } catch (e) {
+            console.error(e)
+        }
+    }
 }
+
 </script>
 
 <style lang='sass'>
