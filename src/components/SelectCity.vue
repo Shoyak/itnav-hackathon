717ad926645b4096fe5a3b6ca99c97298a0458fa
久保昇也 <skhcvhghdyjrt@gmail.com>
2018-02-09 21:55:53 +0900

    [Add] 市町村を選択後、その市町村のwikipediaを表示しリンクを表示させた

--- src/components/SelectCity.vue
+++ src/components/SelectCity.vue
@@ -4,6 +4,7 @@
     select(v-model="selected")
         option(v-for="city in cities" v-bind:value="city.cityCode") {{ city.cityName }}
     span 選択されたprefCode: {{ selected }}
+    a(v-bind:href="searchedCityUrl")  {{ searchedCityUrl }}
     button(@click="check") check
 
     //- citiesの中身を列挙 cityNameを表示
@@ -23,47 +24,48 @@ import VueUtil from '@/scripts/util/VueUtil';
 @Component({})
 export default class SelectCity extends Vue {
     private selected: string = "選択してください";
+    private searchedCityUrl: string = "";
     private cities: Cities = [
-        { "prefCode": 4, "cityCode": "04100", "cityName": "仙台市", "bigCityFlag": "2" },
-        { "prefCode": 4, "cityCode": "04101", "cityName": "仙台市青葉区", "bigCityFlag": "1" },
-        { "prefCode": 4, "cityCode": "04102", "cityName": "仙台市宮城野区", "bigCityFlag": "1" },
-        { "prefCode": 4, "cityCode": "04103", "cityName": "仙台市若林区", "bigCityFlag": "1" },
-        { "prefCode": 4, "cityCode": "04104", "cityName": "仙台市太白区", "bigCityFlag": "1" },
-        { "prefCode": 4, "cityCode": "04105", "cityName": "仙台市泉区", "bigCityFlag": "1" },
-        { "prefCode": 4, "cityCode": "04202", "cityName": "石巻市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04203", "cityName": "塩竈市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04205", "cityName": "気仙沼市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04206", "cityName": "白石市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04207", "cityName": "名取市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04208", "cityName": "角田市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04209", "cityName": "多賀城市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04211", "cityName": "岩沼市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04212", "cityName": "登米市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04213", "cityName": "栗原市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04214", "cityName": "東松島市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04215", "cityName": "大崎市", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04301", "cityName": "蔵王町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04302", "cityName": "七ヶ宿町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04321", "cityName": "大河原町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04322", "cityName": "村田町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04323", "cityName": "柴田町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04324", "cityName": "川崎町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04341", "cityName": "丸森町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04361", "cityName": "亘理町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04362", "cityName": "山元町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04401", "cityName": "松島町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04404", "cityName": "七ヶ浜町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04406", "cityName": "利府町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04421", "cityName": "大和町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04422", "cityName": "大郷町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04423", "cityName": "富谷町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04424", "cityName": "大衡村", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04444", "cityName": "色麻町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04445", "cityName": "加美町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04501", "cityName": "涌谷町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04505", "cityName": "美里町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04581", "cityName": "女川町", "bigCityFlag": "0" },
-        { "prefCode": 4, "cityCode": "04606", "cityName": "南三陸町", "bigCityFlag": "0" }
+        { "cityCode": "04100", "cityName": "仙台市", "cityUrl": "https://ja.wikipedia.org/wiki/%E4%BB%99%E5%8F%B0%E5%B8%82" },
+        { "cityCode": "04101", "cityName": "仙台市青葉区", "cityUrl": "" },
+        { "cityCode": "04102", "cityName": "仙台市宮城野区", "cityUrl": "" },
+        { "cityCode": "04103", "cityName": "仙台市若林区", "cityUrl": "" },
+        { "cityCode": "04104", "cityName": "仙台市太白区", "cityUrl": "" },
+        { "cityCode": "04105", "cityName": "仙台市泉区", "cityUrl": "" },
+        { "cityCode": "04202", "cityName": "石巻市", "cityUrl": "" },
+        { "cityCode": "04203", "cityName": "塩竈市", "cityUrl": "" },
+        { "cityCode": "04205", "cityName": "気仙沼市", "cityUrl": "" },
+        { "cityCode": "04206", "cityName": "白石市", "cityUrl": "" },
+        { "cityCode": "04207", "cityName": "名取市", "cityUrl": "" },
+        { "cityCode": "04208", "cityName": "角田市", "cityUrl": "" },
+        { "cityCode": "04209", "cityName": "多賀城市", "cityUrl": "" },
+        { "cityCode": "04211", "cityName": "岩沼市", "cityUrl": "" },
+        { "cityCode": "04212", "cityName": "登米市", "cityUrl": "" },
+        { "cityCode": "04213", "cityName": "栗原市", "cityUrl": "" },
+        { "cityCode": "04214", "cityName": "東松島市", "cityUrl": "" },
+        { "cityCode": "04215", "cityName": "大崎市", "cityUrl": "" },
+        { "cityCode": "04301", "cityName": "蔵王町", "cityUrl": "" },
+        { "cityCode": "04302", "cityName": "七ヶ宿町", "cityUrl": "" },
+        { "cityCode": "04321", "cityName": "大河原町", "cityUrl": "" },
+        { "cityCode": "04322", "cityName": "村田町", "cityUrl": "" },
+        { "cityCode": "04323", "cityName": "柴田町", "cityUrl": "" },
+        { "cityCode": "04324", "cityName": "川崎町", "cityUrl": "" },
+        { "cityCode": "04341", "cityName": "丸森町", "cityUrl": "" },
+        { "cityCode": "04361", "cityName": "亘理町", "cityUrl": "" },
+        { "cityCode": "04362", "cityName": "山元町", "cityUrl": "" },
+        { "cityCode": "04401", "cityName": "松島町", "cityUrl": "" },
+        { "cityCode": "04404", "cityName": "七ヶ浜町", "cityUrl": "" },
+        { "cityCode": "04406", "cityName": "利府町", "cityUrl": "" },
+        { "cityCode": "04421", "cityName": "大和町", "cityUrl": "" },
+        { "cityCode": "04422", "cityName": "大郷町", "cityUrl": "" },
+        { "cityCode": "04423", "cityName": "富谷町", "cityUrl": "" },
+        { "cityCode": "04424", "cityName": "大衡村", "cityUrl": "" },
+        { "cityCode": "04444", "cityName": "色麻町", "cityUrl": "" },
+        { "cityCode": "04445", "cityName": "加美町", "cityUrl": "" },
+        { "cityCode": "04501", "cityName": "涌谷町", "cityUrl": "" },
+        { "cityCode": "04505", "cityName": "美里町", "cityUrl": "" },
+        { "cityCode": "04581", "cityName": "女川町", "cityUrl": "" },
+        { "cityCode": "04606", "cityName": "南三陸町", "cityUrl": "" }
     ];
 
     private check() {
@@ -78,7 +80,8 @@ export default class SelectCity extends Vue {
             return false;
         });
         // 検索対象のcityCode表示
-        console.log(citySelected[0].cityName);
+        this.searchedCityUrl = citySelected[0].cityUrl;
+        console.log(this.searchedCityUrl);
     }
 }
 </script>
